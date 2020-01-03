9dc2363384567e146642958f3cf4a41e8b51af7a
huangkaining <huangkaininghkn@163.com>
2020-01-03 19:58:22 +0800

    对文件读写做了更新

--- main.py
+++ main.py
@@ -38,103 +38,72 @@ class MainWindow(QMainWindow,Ui_MainWindow):
             if self.ChoicePdfminer.isChecked():
                 pdf2TxtManager = CPdf2TxtManager()
                 if self.ChoiceWriteFile.isChecked():
-                    textresult = pdf2TxtManager.changePdfToText1(self.filePath)
+                    textresult = pdf2TxtManager.changePdfToText(self.filePath,1)
                 else:
                     #print(00)
-                    textresult = pdf2TxtManager.changePdfToText2(self.filePath)
+                    textresult = pdf2TxtManager.changePdfToText(self.filePath,0)
                 self.TextResult.setText(textresult)
 
 
 
 
 class CPdf2TxtManager():
-    def changePdfToText1(self, filePath):
+    def changePdfToText(self, filePath,write_method):
         # 以二进制读模式打开
-        file = open(filePath, 'rb')
-        #用文件对象来创建一个pdf文档分析器
-        praser = PDFParser(file)
-        # 创建一个PDF文档对象存储文档结构,提供密码初始化，没有就不用传该参数
-        doc = PDFDocument(praser, password='')
-        ##检查文件是否允许文本提取
-        if not doc.is_extractable:
-            raise PDFTextExtractionNotAllowed
-        # 创建PDf 资源管理器 来管理共享资源，#caching = False不缓存
-        rsrcmgr = PDFResourceManager(caching = False)
-        # 创建一个PDF设备对象
-        laparams = LAParams()
-        # 创建一个PDF页面聚合对象
-        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-        # 创建一个PDF解析器对象
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        # 获得文档的目录（纲要）,文档没有纲要会报错
-        #PDF文档没有目录时会报：raise PDFNoOutlines  pdfminer.pdfdocument.PDFNoOutlines
-        # print(doc.get_outlines())
-        # 获取page列表
-        #print(PDFPage.get_pages(doc))
-        # 循环遍历列表，每次处理一个page的内容
-        text_result = ""
-        for page in PDFPage.create_pages(doc):
-            interpreter.process_page(page)
-            # 接受该页面的LTPage对象
-            layout = device.get_result()
-            # 这里layout是一个LTPage对象 里面存放着 这个page解析出的各种对象
-            # 一般包括LTTextBox, LTFigure, LTImage, LTTextBoxHorizontal 等等
-            for x in layout:
-                if hasattr(x, "get_text"):
-                    fileNames = os.path.splitext(filePath)
-                    with open(fileNames[0] + '.txt','a+',encoding='utf-8') as f:
-                        results = x.get_text()
-                        text_result = text_result + results
-                        f.write(results + '\n')
-        return text_result
-                # 如果x是水平文本对象的话
-                # if (isinstance(x, LTTextBoxHorizontal)):
-                #     text = re.sub(replace, '', x.get_text())
-                #     if len(text) != 0:
-                #         print(text)
-    def changePdfToText2(self, filePath):
-        # 以二进制读模式打开
-        file = open(filePath, 'rb')
-        #用文件对象来创建一个pdf文档分析器
-        praser = PDFParser(file)
-        # 创建一个PDF文档对象存储文档结构,提供密码初始化，没有就不用传该参数
-        doc = PDFDocument(praser, password='')
-        ##检查文件是否允许文本提取
-        if not doc.is_extractable:
-            raise PDFTextExtractionNotAllowed
-        # 创建PDf 资源管理器 来管理共享资源，#caching = False不缓存
-        rsrcmgr = PDFResourceManager(caching = False)
-        # 创建一个PDF设备对象
-        laparams = LAParams()
-        # 创建一个PDF页面聚合对象
-        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-        # 创建一个PDF解析器对象
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        # 获得文档的目录（纲要）,文档没有纲要会报错
-        #PDF文档没有目录时会报：raise PDFNoOutlines  pdfminer.pdfdocument.PDFNoOutlines
-        # print(doc.get_outlines())
-        # 获取page列表
-        #print(PDFPage.get_pages(doc))
-        # 循环遍历列表，每次处理一个page的内容
-        text_result = ""
-        #print(1)
-        for page in PDFPage.create_pages(doc):
-            interpreter.process_page(page)
-            # 接受该页面的LTPage对象
-            layout = device.get_result()
-            # 这里layout是一个LTPage对象 里面存放着 这个page解析出的各种对象
-            # 一般包括LTTextBox, LTFigure, LTImage, LTTextBoxHorizontal 等等
-            for x in layout:
-                if hasattr(x, "get_text"):
-                    #print(2)
-                    results = x.get_text()
-                    text_result = text_result + results
-        return text_result
-                # 如果x是水平文本对象的话
-                # if (isinstance(x, LTTextBoxHorizontal)):
-                #     text = re.sub(replace, '', x.get_text())
-                #     if len(text) != 0:
-                #         print(text)
+        file = None
+        try:
+            file = open(filePath, 'rb')
+            #用文件对象来创建一个pdf文档分析器
+            praser = PDFParser(file)
+            # 创建一个PDF文档对象存储文档结构,提供密码初始化，没有就不用传该参数
+            doc = PDFDocument(praser, password='')
+            ##检查文件是否允许文本提取
+            if not doc.is_extractable:
+                raise PDFTextExtractionNotAllowed
+            # 创建PDf 资源管理器 来管理共享资源，#caching = False不缓存
+            rsrcmgr = PDFResourceManager(caching = False)
+            # 创建一个PDF设备对象
+            laparams = LAParams()
+            # 创建一个PDF页面聚合对象
+            device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+            # 创建一个PDF解析器对象
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            # 获得文档的目录（纲要）,文档没有纲要会报错
+            #PDF文档没有目录时会报：raise PDFNoOutlines  pdfminer.pdfdocument.PDFNoOutlines
+            # print(doc.get_outlines())
+            # 获取page列表
+            #print(PDFPage.get_pages(doc))
+            # 循环遍历列表，每次处理一个page的内容
+            text_result = ""
+            for page in PDFPage.create_pages(doc):
+                interpreter.process_page(page)
+                # 接受该页面的LTPage对象
+                layout = device.get_result()
+                # 这里layout是一个LTPage对象 里面存放着 这个page解析出的各种对象
+                # 一般包括LTTextBox, LTFigure, LTImage, LTTextBoxHorizontal 等等
+                for x in layout:
+                    if hasattr(x, "get_text"):
+                        if write_method == 1:
+                            fileNames = os.path.splitext(filePath)
+                            with open(fileNames[0] + '.txt','a+',encoding='utf-8') as f:
+                                results = x.get_text()
+                                text_result = text_result + results
+                                f.write(results + '\n')
+                        elif write_method == 0:
+                            results = x.get_text()
+                            text_result = text_result + results
+            file.close()
+            return text_result
+            # 如果x是水平文本对象的话
+            # if (isinstance(x, LTTextBoxHorizontal)):
+            #     text = re.sub(replace, '', x.get_text())
+            #     if len(text) != 0:
+            #         print(text)
+        finally:
+            if file:
+                file.close()
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
